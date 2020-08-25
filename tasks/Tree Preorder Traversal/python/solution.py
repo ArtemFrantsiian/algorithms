04c0715cd4e563750e971cb1bc5819537492ad41
ArtemFrantsiian <artemfrantsiian@gmail.com>
2020-08-25 13:20:47 +0300

    add solution for preorder traversal

--- tasks/Tree Preorder Traversal/python/solution.py
+++ tasks/Tree Preorder Traversal/python/solution.py
@@ -8,28 +8,28 @@ class Node:
         self.children = children
 
 
-def preorder_recursion(root: Node) -> List[int]:
-    if not root:
-        return []
+def preorder(root: Node) -> List[int]:
+    stack = [root]
 
-    result = [root.val]
+    result = []
+    while stack:
+        node = stack.pop()
+        if node:
+            result.append(node.val)
+            if node.children:
+                stack.extend(node.children[::-1])
 
-    if not root.children:
-        return result
-
-    for child in root.children:
-        result.extend(preorder_recursion(child))
     return result
 
 
 class Test(unittest.TestCase):
     def test1(self):
         node = Node(1, [Node(3, [Node(5), Node(6)]), Node(2), Node(4)])
-        self.assertEqual(preorder_recursion(node), [1, 3, 5, 6, 2, 4])
+        self.assertEqual(preorder(node), [1, 3, 5, 6, 2, 4])
 
     def test2(self):
         node = Node(1, [Node(2), Node(3, [Node(6), Node(7, [Node(11, [Node(14)])])]), Node(4, [Node(8, [Node(12)])]), Node(5, [Node(9, [Node(13)]), Node(10)])])
-        self.assertEqual(preorder_recursion(node), [1, 2, 3, 6, 7, 11, 14, 4, 8, 12, 5, 9, 13, 10])
+        self.assertEqual(preorder(node), [1, 2, 3, 6, 7, 11, 14, 4, 8, 12, 5, 9, 13, 10])
 
 
 if __name__ == "__main__":
